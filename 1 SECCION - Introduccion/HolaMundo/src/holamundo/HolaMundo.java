e39484578e1eddfecad5e357129e127a6dd2e404
jreyes1990 <jreyes1990mt@gmail.com>
2023-11-26 19:24:10 -0600

    7. Ejemplo Hola Mundo en Java

---
+++ 1 SECCION - Introduccion/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+/**
+ *
+ * @author jarl1
+ */
+public class HolaMundo {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("Hola mundo desde java");
+  }
+}
