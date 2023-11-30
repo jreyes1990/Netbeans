3af6d774fe2c421f7906e56972131ddef1fcd5aa
jreyes1990 <jreyes1990mt@gmail.com>
2023-11-29 22:54:51 -0600

    13. Primitivos numéricos de punto flotante

---
+++ 2 SECCION - Variables/Variables/src/primitivos/PrimitivosFloat.java
@@ -0,0 +1,15 @@
+package primitivos;
+
+public class PrimitivosFloat {
+
+  public static void main(String[] args) {
+    System.out.println("********** PRIMITIVOS NUMERICOS DE PUNTO FLOAT **********");
+    System.out.println("\n********** FLOAT **********");
+    float numeroFloat = 2120.25F;
+    System.out.println("numero float: "+numeroFloat);
+    System.out.println("tipo float corresponde en byte a "+Float.BYTES);
+    System.out.println("tipo float corresponde en bites a "+Float.SIZE);
+    System.out.println("Valor minimo de un float "+Float.MIN_VALUE);
+    System.out.println("Valor maximo de un float "+Float.MAX_VALUE);
+  }  
+}
