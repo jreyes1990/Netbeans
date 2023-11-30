ed21ce2ec2690fa8b1a406641c939013b16f1d49
jreyes1990 <jreyes1990mt@gmail.com>
2023-11-29 23:07:46 -0600

    14. Primitivos numéricos de punto flotante y sus constantes

--- 2 SECCION - Variables/Variables/src/primitivos/PrimitivosFloat.java
+++ 2 SECCION - Variables/Variables/src/primitivos/PrimitivosFloat.java
@@ -1,6 +1,7 @@
 package primitivos;
 
 public class PrimitivosFloat {
+  static float varFlotante=45.45F;
 
   public static void main(String[] args) {
     System.out.println("********** PRIMITIVOS NUMERICOS DE PUNTO FLOAT **********");
@@ -11,5 +12,16 @@ public class PrimitivosFloat {
     System.out.println("tipo float corresponde en bites a "+Float.SIZE);
     System.out.println("Valor minimo de un float "+Float.MIN_VALUE);
     System.out.println("Valor maximo de un float "+Float.MAX_VALUE);
+    
+    System.out.println("\n********** DOUBLE **********");
+    double numeroDouble = 2120.252566255;
+    System.out.println("numero double: "+numeroDouble);
+    System.out.println("tipo double corresponde en byte a "+Double.BYTES);
+    System.out.println("tipo double corresponde en bites a "+Double.SIZE);
+    System.out.println("Valor minimo de un double "+Double.MIN_VALUE);
+    System.out.println("Valor maximo de un double "+Double.MAX_VALUE);
+    
+    System.out.println("\n********** VAR FLOTANTE **********");
+    System.out.println("numero var flotante: "+varFlotante);
   }  
 }
