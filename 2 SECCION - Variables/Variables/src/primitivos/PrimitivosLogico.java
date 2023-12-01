cb397f60ae5a7b37b2c42ffd3a8b1b115a31557c
jreyes1990 <jreyes1990mt@gmail.com>
2023-11-30 22:58:19 -0600

    17. Primitivo boolean o lógico binario

---
+++ 2 SECCION - Variables/Variables/src/primitivos/PrimitivosLogico.java
@@ -0,0 +1,11 @@
+package primitivos;
+
+public class PrimitivosLogico {
+
+  public static void main(String[] args) {
+    System.out.println("********** PRIMITIVOS LOGICO **********");
+    boolean datoLogico = true;
+    
+    System.out.println("datoLogico = "+datoLogico);
+  }  
+}
