84fdd28b7b9e730d735c5639317f351c2440975b
jreyes1990 <jreyes1990mt@gmail.com>
2023-12-03 18:08:14 -0600

    30. Validar un String

--- Andres Guzman/3 SECCION - Tipo String (Cadenas)/TipoString/src/tipostring/StringValidar.java
+++ Andres Guzman/3 SECCION - Tipo String (Cadenas)/TipoString/src/tipostring/StringValidar.java
@@ -3,9 +3,27 @@ package tipostring;
 public class StringValidar {
 
   public static void main(String[] args) {
+    System.out.println("********** TIPO STRING - VALIDACION **********");
     String curso = null;
     boolean esNulo = curso == null;
     System.out.println("esNulo = "+esNulo);
-    System.out.println(curso.toUpperCase());
+    
+    System.out.println("\n********** Validacion con if **********");
+    if (esNulo) {
+      curso = "";
+    }
+    
+    System.out.println("\n********** Validacion con length **********");
+    boolean esVacio = curso.length() == 0;
+    System.out.println("esVacio = "+esVacio);
+    
+    System.out.println("\n********** Validacion con isEmpty **********");
+    boolean esVacio2 = curso.isEmpty();
+    System.out.println("esVacio2 = "+esVacio2);
+    
+    if (!esVacio2) {
+      System.out.println(curso.toUpperCase());
+      System.out.println("Bienvenido al curso ".concat(curso));
+    }
   }  
 }
