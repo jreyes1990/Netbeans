af6ca1dd885bfb41f8d7418d497faac185bd4e70
jreyes1990 <jreyes1990mt@gmail.com>
2023-12-04 22:36:51 -0600

    32. Métodos importantes del objeto String parte 2

--- Andres Guzman/3 SECCION - Tipo String (Cadenas)/TipoString/src/tipostring/StringMetodos.java
+++ Andres Guzman/3 SECCION - Tipo String (Cadenas)/TipoString/src/tipostring/StringMetodos.java
@@ -3,7 +3,7 @@ package tipostring;
 public class StringMetodos {
 
   public static void main(String[] args) {
-    System.out.println("********** TIPO STRING - METODOS **********");
+    System.out.println("********** TIPO STRING - METODOS PARTE 1 **********");
     String nombre = "Jolberth";
     
     System.out.println("\n********** metodo length() **********");
@@ -33,5 +33,35 @@ public class StringMetodos {
     System.out.println("nombre.substring(1) = "+nombre.substring(1));
     System.out.println("nombre.substring(1, 4) = "+nombre.substring(1, 4));
     System.out.println("nombre.substring(nombre.length()-1) = "+nombre.substring(nombre.length()-1));
+    
+    System.out.println("********** TIPO STRING - METODOS PARTE 2 **********");
+    String trabalenguas = "trabalenguas";
+    
+    System.out.println("\n********** metodo replace() **********");
+    System.out.println("trabalenguas.replace(\"a\", \".\") = "+trabalenguas.replace("a", "."));
+    
+    System.out.println("\n********** metodo indexOf() **********");
+    System.out.println("trabalenguas.indexOf(\"a\") = "+trabalenguas.indexOf("a"));
+    System.out.println("trabalenguas.indexOf(\"e\") = "+trabalenguas.indexOf("e"));
+    System.out.println("trabalenguas.indexOf(\"z\") = "+trabalenguas.indexOf("z"));
+    
+    System.out.println("\n********** metodo lastIndexOf() **********");
+    System.out.println("trabalenguas.lastIndexOf(\"a\") = "+trabalenguas.lastIndexOf("a"));
+    
+    System.out.println("\n********** metodo contains() **********");
+    System.out.println("trabalenguas.contains(\"t\") = "+trabalenguas.contains("t"));
+    System.out.println("trabalenguas.contains(\"o\") = "+trabalenguas.contains("o"));
+    
+    System.out.println("\n********** metodo startsWith() **********");
+    System.out.println("trabalenguas.startsWith(\"lenguas\") = "+trabalenguas.startsWith("lenguas"));
+    System.out.println("trabalenguas.startsWith(\"trab\") = "+trabalenguas.startsWith("trab"));
+    
+    System.out.println("\n********** metodo endsWith() **********");
+    System.out.println("trabalenguas.endsWith(\"lenguas\") = "+trabalenguas.endsWith("lenguas"));
+    System.out.println("trabalenguas.endsWith(\"trab\") = "+trabalenguas.endsWith("trab"));
+    
+    System.out.println("\n********** metodo trim() **********");
+    System.out.println("   trabalenguas   ");
+    System.out.println("\"   trabalenguas   \".trim()".trim());
   }  
 }
