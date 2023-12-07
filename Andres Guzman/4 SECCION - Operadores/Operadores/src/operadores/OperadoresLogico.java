6589fd1165ec83b54dd113eb8517f4f6133aeeb9
jreyes1990 <jreyes1990mt@gmail.com>
2023-12-06 23:29:58 -0600

    42. Precedencia en los operadores lógicos

--- Andres Guzman/4 SECCION - Operadores/Operadores/src/operadores/OperadoresLogico.java
+++ Andres Guzman/4 SECCION - Operadores/Operadores/src/operadores/OperadoresLogico.java
@@ -18,5 +18,18 @@ public class OperadoresLogico {
     
     boolean b3 = i==j && k>l || m==false;
     System.out.println("b3 = "+b3);
+    
+    System.out.println("********** OPERADORES - LOGICO - PRECEDENCIA **********");
+    boolean b4 = (i==j || k<l) && m==true;
+    System.out.println("b4 = "+b4);
+    
+    boolean b5 = (true || true) && false;
+    System.out.println("b5 = "+b5);
+    
+    boolean b6 = ((true || false) && false) || false;
+    System.out.println("b6 = "+b6);
+    
+    boolean b7 = true || (false && false) || false;
+    System.out.println("b7 = "+b7);
   }  
 }
