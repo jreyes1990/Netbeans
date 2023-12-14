95786aa87e61d0781a4bec040eee9ccf541cbfac
jreyes1990 <jarl1990mt@gmail.com>
2023-12-13 21:43:46 -0600

    61. Flujo de control while y do while

---
+++ Andres Guzman/5 SECCION - Flujos de Control/FlujoControl/src/flujocontrol/While.java
@@ -0,0 +1,41 @@
+package flujocontrol;
+
+public class While {
+
+  public static void main(String[] args) {
+    System.out.println("PRIMER CODIGO WHILE");
+    int i = 0;
+    while (i < 5) {      
+      System.out.println("i = "+i);
+      i++;
+    }
+    
+    System.out.println("SEGUNDO CODIGO WHILE");
+    i = 0;
+    boolean prueba = true;
+    while (prueba) {      
+      if (i == 7) {
+        prueba = false;
+      }
+      System.out.println("i = "+i);
+      i++;
+    }
+    
+    System.out.println("\nPRIMER CODIGO DO WHILE");
+    prueba = false;
+    do {      
+      System.out.println("Se ejecuta al menos una vez");
+    } while (prueba);
+    
+    System.out.println("SEGUNDO CODIGO DO WHILE");
+    prueba = true;
+    i = 0;
+    do {      
+      if (i == 10) {
+        prueba = false;
+      }
+      System.out.println("i = "+i);
+      i++;
+    } while (prueba);
+  }  
+}
