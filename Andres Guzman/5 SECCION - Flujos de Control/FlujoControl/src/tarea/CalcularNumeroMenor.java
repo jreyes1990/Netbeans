6945e160374a7a03433189fcd0199b287728bb56
jreyes1990 <jarl1990mt@gmail.com>
2023-12-17 13:08:27 -0600

    Tarea: Calcular el numero menor de multiples valores

--- Andres Guzman/5 SECCION - Flujos de Control/FlujoControl/src/tarea/CalcularNumeroMenor.java
+++ Andres Guzman/5 SECCION - Flujos de Control/FlujoControl/src/tarea/CalcularNumeroMenor.java
@@ -23,31 +23,31 @@ public class CalcularNumeroMenor {
     // Verificar que la cantidad de números sea al menos 1
     if (cantidadNumeros < 1) {
       System.out.println("Debe ingresar al menos un número.");
-      return;
-    }
-
-    // Inicializar la variable para almacenar el menor número
-    int menorNumero = Integer.MAX_VALUE;
-
-    // Iterar para ingresar los números y encontrar el menor
-    for (int i = 1; i <= cantidadNumeros; i++) {
-      System.out.print("Ingrese el número #" + i + ": ");
-      int numero = scanner.nextInt();
-
-      // Actualizar el menor número si es necesario
-      if (numero < menorNumero) {
-        menorNumero = numero;
-      }
-    }
-
-    // Imprimir el menor número
-    System.out.println("El número menor es: " + menorNumero);
-
-    // Verificar si el menor número es menor que 10
-    if (menorNumero < 10) {
-      System.out.println("El número menor es menor que 10!");
+      main(args);
     } else {
-      System.out.println("El número menor es igual o mayor que 10!");
+      // Inicializar la variable para almacenar el menor número
+      int menorNumero = Integer.MAX_VALUE;
+
+      // Iterar para ingresar los números y encontrar el menor
+      for (int i = 1; i <= cantidadNumeros; i++) {
+        System.out.print("Ingrese el número #" + i + ": ");
+        int numero = scanner.nextInt();
+
+        // Actualizar el menor número si es necesario
+        if (numero < menorNumero) {
+          menorNumero = numero;
+        }
+      }
+
+      // Imprimir el menor número
+      System.out.println("El número menor es: " + menorNumero);
+
+      // Verificar si el menor número es menor que 10
+      if (menorNumero < 10) {
+        System.out.println("El número menor es menor que 10!");
+      } else {
+        System.out.println("El número menor es igual o mayor que 10!");
+      }
     }
 
     // Cerrar el scanner
