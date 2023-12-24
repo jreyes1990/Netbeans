040db42cc90c979f4a10310b4601f6b049056c75
jreyes1990 <jarl1990mt@gmail.com>
2023-12-23 22:24:30 -0600

    78. La clase Calendar

---
+++ Andres Guzman/8 SECCION - Date y Calendar/Date_y_Calendar/src/date_y_calendar/EjemploCalendar.java
@@ -0,0 +1,31 @@
+package date_y_calendar;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class EjemploCalendar {
+
+  public static void main(String[] args) {
+    Calendar calendario = Calendar.getInstance();
+    // calendario.set(2020, 0, 25);
+    // calendario.set(2020, Calendar.SEPTEMBER, 25, 18, 20, 10);
+    calendario.set(Calendar.YEAR, 2020);
+    calendario.set(Calendar.MONTH, Calendar.JULY);
+    calendario.set(Calendar.DAY_OF_MONTH, 25);
+    
+    // calendario.set(Calendar.HOUR_OF_DAY, 21);
+    calendario.set(Calendar.HOUR, 7);
+    calendario.set(Calendar.AM_PM, Calendar.AM);
+    calendario.set(Calendar.MINUTE, 20);
+    calendario.set(Calendar.SECOND, 10);
+    calendario.set(Calendar.MILLISECOND, 125);
+    
+    Date fechaSinFormato = calendario.getTime();
+    System.out.println("fechaSinFormato = "+fechaSinFormato);
+    
+    SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS a");
+    String fechaConFormato = formato.format(fechaSinFormato);
+    System.out.println("fechaConFormato = "+fechaConFormato);
+  }  
+}
