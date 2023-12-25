bbb7e7691ca5e03a418e113d32800360b7276329
jreyes1990 <jarl1990mt@gmail.com>
2023-12-25 17:20:06 -0600

    87. La clase Math parte 2 (trigonometría)

--- Andres Guzman/10 SECCION - Clase Math/ClaseMath/src/clasemath/ClaseMath.java
+++ Andres Guzman/10 SECCION - Clase Math/ClaseMath/src/clasemath/ClaseMath.java
@@ -20,5 +20,25 @@ public class ClaseMath {
     
     double entero = Math.round(3.5);
     System.out.println("round = "+entero);
+    
+    double exponente = Math.exp(1);
+    System.out.println("exp = "+exponente);
+    
+    double logaritmoNatural = Math.log(10);
+    System.out.println("log = "+logaritmoNatural);
+    
+    double potencia = Math.pow(10, 3);
+    System.out.println("pow = "+potencia);
+    
+    double raizCuadrada = Math.sqrt(5);
+    System.out.println("sqrt = "+raizCuadrada);
+    
+    double convertirRadianesToGrados = Math.toDegrees(1.57);
+    convertirRadianesToGrados = Math.round(convertirRadianesToGrados);
+    System.out.println("toDegrees = "+convertirRadianesToGrados);
+    
+    double convertirGradosToRadianes = Math.toRadians(90.00);
+    convertirGradosToRadianes = Math.round(convertirGradosToRadianes);
+    System.out.println("toRadians = "+convertirGradosToRadianes);
   }  
 }
