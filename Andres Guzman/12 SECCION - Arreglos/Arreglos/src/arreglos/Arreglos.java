f5bc995504a59ca79dc45d56edbe2bc54a40cf62
jreyes1990 <jarl1990mt@gmail.com>
2023-12-27 22:34:47 -0600

    95. Ejemplo de un arreglo de string y ordenar con sort

--- Andres Guzman/12 SECCION - Arreglos/Arreglos/src/arreglos/Arreglos.java
+++ Andres Guzman/12 SECCION - Arreglos/Arreglos/src/arreglos/Arreglos.java
@@ -1,14 +1,19 @@
 package arreglos;
 
+import java.util.Arrays;
+
 public class Arreglos {
 
   public static void main(String[] args) {
     int[] numeros = new int[4];
     
-    numeros[0] = 1;
-    numeros[1] = Integer.valueOf("2");
-    numeros[2] = 3;
-    numeros[3] = 4;
+    numeros[0] = 10;
+    numeros[1] = Integer.valueOf("7");
+    numeros[2] = 35;
+    numeros[3] = -1;
+    
+    // Ordena los productos, segun numero
+    Arrays.sort(numeros);
     
     int i = numeros[0];
     int j = numeros[1];
@@ -19,5 +24,32 @@ public class Arreglos {
     System.out.println("j = "+j);
     System.out.println("k = "+k);
     System.out.println("l = "+l);
+    
+    System.out.println();
+    
+    String[] productos = new String[7];
+    
+    productos[0] = "Kingston Pendrive 64GB";
+    productos[1] = "Samsung Galaxy";
+    productos[2] = "Disco Duro SSD Samsung Externo";
+    productos[3] = "Asus Notebook";
+    productos[4] = "Macbook Air";
+    productos[5] = "Chromecast 4ta Generacion";
+    productos[6] = "Bicicleta Oxford";
+    
+    // Ordena los productos, segun su descripcion
+    Arrays.sort(productos);
+    
+    
+    System.out.println("productos[0] = "+productos[0]);
+    System.out.println("productos[1] = "+productos[1]);
+    System.out.println("productos[2] = "+productos[2]);
+    System.out.println("productos[3] = "+productos[3]);
+    
+    System.out.println();
+    
+    for (int m = 0; m < productos.length; m++) {
+      System.out.println("productos["+m+"] = "+productos[m]);
+    }
   }  
 }
