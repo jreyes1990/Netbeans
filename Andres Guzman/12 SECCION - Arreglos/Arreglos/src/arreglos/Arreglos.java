6da42ac8de724de20c2e4d0458419eeb3f378847
jreyes1990 <jarl1990mt@gmail.com>
2023-12-27 22:16:37 -0600

    94. Ejemplo de un arreglo de enteros

---
+++ Andres Guzman/12 SECCION - Arreglos/Arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,23 @@
+package arreglos;
+
+public class Arreglos {
+
+  public static void main(String[] args) {
+    int[] numeros = new int[4];
+    
+    numeros[0] = 1;
+    numeros[1] = Integer.valueOf("2");
+    numeros[2] = 3;
+    numeros[3] = 4;
+    
+    int i = numeros[0];
+    int j = numeros[1];
+    int k = numeros[2];
+    int l = numeros[numeros.length-1];
+    
+    System.out.println("i = "+i);
+    System.out.println("j = "+j);
+    System.out.println("k = "+k);
+    System.out.println("l = "+l);
+  }  
+}
