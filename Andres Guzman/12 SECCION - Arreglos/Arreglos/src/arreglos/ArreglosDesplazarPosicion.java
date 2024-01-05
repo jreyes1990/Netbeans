9f851c3465b5d64884e415f78e4d00ada06b63a9
jreyes1990 <jarl1990mt@gmail.com>
2024-01-04 21:06:56 -0600

    110. Mover los elementos en una posición desde el inicio del arreglo hasta el final

---
+++ Andres Guzman/12 SECCION - Arreglos/Arreglos/src/arreglos/ArreglosDesplazarPosicion.java
@@ -0,0 +1,30 @@
+package arreglos;
+
+import java.util.Scanner;
+
+public class ArreglosDesplazarPosicion {
+
+  public static void main(String[] args) {
+    int[] a = new int[10];
+    int ultimo;
+    Scanner s = new Scanner(System.in);
+    
+    for (int i = 0; i < a.length; i++) {
+      System.out.print("["+i+"] Ingrese el numero: ");
+      a[i] = s.nextInt();
+    }
+    
+    System.out.println();
+    ultimo = a[a.length-1];
+    
+    for (int i = a.length-2; i >= 0; i--) {
+      a[i+1] = a[i];
+    }
+    a[0] = ultimo;
+    
+    System.out.println("El arreglo: ");
+    for (int i = 0; i < a.length; i++) {
+      System.out.println("a["+i+"] = "+a[i]);
+    }
+  }  
+}
