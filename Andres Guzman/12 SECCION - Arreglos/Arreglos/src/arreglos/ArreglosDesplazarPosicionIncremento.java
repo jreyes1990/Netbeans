edf1c31d844de4604df55b4eb740a6719c8ee80c
jreyes1990 <jarl1990mt@gmail.com>
2024-01-05 14:55:27 -0600

    114. Insertando un nuevo elemento e incrementando el tamaño del arreglo

---
+++ Andres Guzman/12 SECCION - Arreglos/Arreglos/src/arreglos/ArreglosDesplazarPosicionIncremento.java
@@ -0,0 +1,45 @@
+package arreglos;
+
+import java.util.Scanner;
+
+public class ArreglosDesplazarPosicionIncremento {
+
+  public static void main(String[] args) {
+    int[] a = new int[7];
+    int numero, posicion, ultimo;
+    Scanner s = new Scanner(System.in);
+    
+    for (int i = 0; i < a.length; i++) {
+      System.out.print("["+i+"] Ingrese el numero: ");
+      a[i] = s.nextInt();
+    }
+    
+    System.out.println();
+    System.out.print("Ingrese un numero a insertar: ");
+    numero = s.nextInt();
+    
+    ultimo = a[a.length-1];
+    posicion = 0;
+    while (posicion < 6 && numero > a[posicion]) {      
+      posicion++;
+    }
+    
+    for (int i = a.length-2; i >= posicion; i--) {
+      a[i+1] = a[i];
+    }
+    int[] b = new int[a.length+1];
+    System.arraycopy(a, 0, b, 0, a.length);
+    
+    if (numero > ultimo) {
+      b[b.length-1] = numero;
+    } else {
+      b[b.length-1] = ultimo;
+      b[posicion] = numero;
+    }
+    
+    System.out.println("Nuevo arreglo ordenado: ");
+    for (int i = 0; i < b.length; i++) {
+      System.out.println("b["+i+"] = "+b[i]);
+    }
+  }  
+}
