4bc4278af5c4421e14b1cccce9d04c9ea72fe7b1
jreyes1990 <jarl1990mt@gmail.com>
2024-01-06 17:31:29 -0600

    125. Operaciones aritméticas en matrices

---
+++ Andres Guzman/13 SECCION - Matrices, Arreglos Bidimensionales/appMatrices/src/appmatrices/MatricesOperacionAritmetica.java
@@ -0,0 +1,51 @@
+package appmatrices;
+
+public class MatricesOperacionAritmetica {
+
+  public static void main(String[] args) {
+    int[][] a = {
+      {1,2,3}, 
+      {4,5,6}, 
+      {7,8,9}
+    };
+    
+    int[][] b = {
+      {10,20,30}, 
+      {40,50,60}, 
+      {70,80,90}
+    };
+    
+    int[][] suma = new int[3][3];
+    
+    System.out.println("MATRIZ de 'A'");
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[i].length; j++) {
+        System.out.print(a[i][j]+"\t");
+      }
+      System.out.println();
+    }
+    
+    System.out.println();
+    System.out.println("MATRIZ de 'B'");
+    for (int i = 0; i < b.length; i++) {
+      for (int j = 0; j < b[i].length; j++) {
+        System.out.print(b[i][j]+"\t");
+      }
+      System.out.println();
+    }
+    
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a[i].length; j++) {
+        suma[i][j] = a[i][j]+b[i][j];
+      }
+    }
+    
+    System.out.println("\nSUMA MATRIZ 'A'+'B'");
+    for (int i = 0; i < suma.length; i++) {
+      for (int j = 0; j < suma[i].length; j++) {
+        System.out.print(suma[i][j]+"\t");
+      }
+      System.out.println();
+    }
+  }  
+}
