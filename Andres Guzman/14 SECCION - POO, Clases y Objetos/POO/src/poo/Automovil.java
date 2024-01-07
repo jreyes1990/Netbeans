fb8e0f9f806040f8d0d46fc3289d3d0fef82b800
jreyes1990 <jarl1990mt@gmail.com>
2024-01-06 22:18:08 -0600

    133. Métodos con argumentos

--- Andres Guzman/14 SECCION - POO, Clases y Objetos/POO/src/poo/Automovil.java
+++ Andres Guzman/14 SECCION - POO, Clases y Objetos/POO/src/poo/Automovil.java
@@ -6,14 +6,29 @@ public class Automovil {
   String color;
   double cilindrada;
   
-  public String detalle(){
+  public String verDetalle(){
     StringBuilder sb = new StringBuilder();
     
-    sb.append("auto.fabricante = "+this.fabricante);
-    sb.append("\nauto.modelo = "+this.modelo);
-    sb.append("\nauto.color = "+this.color);
-    sb.append("\nauto.cilindrada = "+this.cilindrada);
+    sb.append("auto.fabricante = ").append(this.fabricante);
+    sb.append("\nauto.modelo = ").append(this.modelo);
+    sb.append("\nauto.color = ").append(this.color);
+    sb.append("\nauto.cilindrada = ").append(this.cilindrada);
     
     return sb.toString();
+  }
+  
+  public String acelerar(int rpm){
+    return "el auto "+this.fabricante+" acelerando a "+rpm+"rpm";
+  }
+  
+  public String frenar(){
+    return this.fabricante+" "+this.modelo+" frenando!.";
+  }
+  
+  public String acelerarFrenar(int rpm){
+    String acelerar = this.acelerar(rpm);
+    String frenar = this.frenar();
+    
+    return acelerar+"\n"+frenar;
   }
 }
--- Andres Guzman/14 SECCION - POO, Clases y Objetos/POO/src/poo/main.java
+++ Andres Guzman/14 SECCION - POO, Clases y Objetos/POO/src/poo/main.java
@@ -16,8 +16,11 @@ public class main {
     mazda.color = "Rojo";
     mazda.cilindrada = 3.0;
     
-    System.out.println(auto.detalle());
+    System.out.println(auto.verDetalle());
+    System.out.println(auto.acelerar(3000));
+    System.out.println(auto.frenar());
     System.out.println();
-    System.out.println(mazda.detalle());
+    System.out.println(mazda.verDetalle());
+    System.out.println(mazda.acelerarFrenar(8000));
   }  
 }
