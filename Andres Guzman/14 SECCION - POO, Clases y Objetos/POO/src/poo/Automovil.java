38ce86851e76098c4f6089f48199b76be3b5e42c
jreyes1990 <jarl1990mt@gmail.com>
2024-01-06 21:54:30 -0600

    132. Agregando métodos

--- Andres Guzman/14 SECCION - POO, Clases y Objetos/POO/src/poo/Automovil.java
+++ Andres Guzman/14 SECCION - POO, Clases y Objetos/POO/src/poo/Automovil.java
@@ -5,4 +5,15 @@ public class Automovil {
   String modelo;
   String color;
   double cilindrada;
+  
+  public String detalle(){
+    StringBuilder sb = new StringBuilder();
+    
+    sb.append("auto.fabricante = "+this.fabricante);
+    sb.append("\nauto.modelo = "+this.modelo);
+    sb.append("\nauto.color = "+this.color);
+    sb.append("\nauto.cilindrada = "+this.cilindrada);
+    
+    return sb.toString();
+  }
 }
--- Andres Guzman/14 SECCION - POO, Clases y Objetos/POO/src/poo/main.java
+++ Andres Guzman/14 SECCION - POO, Clases y Objetos/POO/src/poo/main.java
@@ -16,15 +16,8 @@ public class main {
     mazda.color = "Rojo";
     mazda.cilindrada = 3.0;
     
-    System.out.println("auto.fabricante = "+auto.fabricante);
-    System.out.println("auto.modelo = "+auto.modelo);
-    System.out.println("auto.color = "+auto.color);
-    System.out.println("auto.cilindrada = "+auto.cilindrada);
-    
+    System.out.println(auto.detalle());
     System.out.println();
-    System.out.println("mazda.fabricante = "+mazda.fabricante);
-    System.out.println("mazda.modelo = "+mazda.modelo);
-    System.out.println("mazda.color = "+mazda.color);
-    System.out.println("mazda.cilindrada = "+mazda.cilindrada);
+    System.out.println(mazda.detalle());
   }  
 }
