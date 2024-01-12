65ac97c7827792b9d156a41f79fbc2f0ec823406
jreyes1990 <jarl1990mt@gmail.com>
2024-01-11 23:19:37 -0600

    165. Creando las clases del modelo

---
+++ Andres Guzman/16 SECCION - POO, Ejemplo Factura/AppFactura/src/org/jreyes/appfacturas/modelo/ItemFactura.java
@@ -0,0 +1,30 @@
+package org.jreyes.appfacturas.modelo;
+
+public class ItemFactura {
+  private int cantidad;
+  // Asociacion de modelos
+  private Producto producto;
+
+  // Contructor
+  public ItemFactura(int cantidad, Producto producto) {
+    this.cantidad = cantidad;
+    this.producto = producto;
+  }
+  
+  public int getCantidad() {
+    return cantidad;
+  }
+
+  public void setCantidad(int cantidad) {
+    this.cantidad = cantidad;
+  }
+
+  public Producto getProducto() {
+    return producto;
+  }
+
+  public void setProducto(Producto producto) {
+    this.producto = producto;
+  }
+  
+}
