0db0351953cddf5f3d76b3613d4059ef9782de83
jreyes1990 <jarl1990mt@gmail.com>
2024-01-12 21:50:32 -0600

    169. Optimizando la aplicación

--- Andres Guzman/16 SECCION - POO, Ejemplo Factura/AppFactura/src/org/jreyes/appfacturas/AppFactura.java
+++ Andres Guzman/16 SECCION - POO, Ejemplo Factura/AppFactura/src/org/jreyes/appfacturas/AppFactura.java
@@ -2,10 +2,7 @@ package org.jreyes.appfacturas;
 
 import java.util.Locale;
 import java.util.Scanner;
-import org.jreyes.appfacturas.modelo.Cliente;
-import org.jreyes.appfacturas.modelo.Factura;
-import org.jreyes.appfacturas.modelo.ItemFactura;
-import org.jreyes.appfacturas.modelo.Producto;
+import org.jreyes.appfacturas.modelo.*;
 
 public class AppFactura {
 
@@ -16,31 +13,22 @@ public class AppFactura {
     
     Scanner s = new Scanner(System.in).useLocale(Locale.US);
     System.out.print("Ingrese una descripcion de la factura: ");
-    String desc = s.nextLine();
-    Factura factura = new Factura(desc, cliente);
+    Factura factura = new Factura(s.nextLine(), cliente);
     
     Producto producto;
-    String nombre;
-    float precio;
-    int cantidad;
-    
     System.out.println();
     
     for (int i = 0; i < 5; i++) {
       producto = new Producto();
       System.out.print("Ingrese producto No. "+producto.getCodigo()+": ");
-      nombre = s.next();
-      producto.setNombre(nombre);
+      producto.setNombre(s.next());
       
       System.out.print("Ingrese el precio: ");
-      precio = s.nextFloat();
-      producto.setPrecio(precio);
+      producto.setPrecio(s.nextFloat());
       
       System.out.print("Ingrese la cantidad: ");
-      cantidad = s.nextInt();
       
-      ItemFactura item = new ItemFactura(cantidad, producto);
-      factura.addItemFactura(item);
+      factura.addItemFactura(new ItemFactura(s.nextInt(), producto));
       System.out.println();
     }
     System.out.println(factura.generarDetalle());
