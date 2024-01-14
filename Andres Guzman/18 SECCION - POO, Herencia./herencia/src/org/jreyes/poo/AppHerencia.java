8fa69fddfd37f85aa8c967e747498eeb8d251b07
jreyes1990 <jarl1990mt@gmail.com>
2024-01-14 17:51:27 -0600

    181. Herencia y el API de reflexión de java

--- Andres Guzman/18 SECCION - POO, Herencia./herencia/src/org/jreyes/poo/AppHerencia.java
+++ Andres Guzman/18 SECCION - POO, Herencia./herencia/src/org/jreyes/poo/AppHerencia.java
@@ -1,6 +1,7 @@
 package org.jreyes.poo;
 
 import org.jreyes.poo.herencia.Alumno;
+import org.jreyes.poo.herencia.AlumnoInternacional;
 import org.jreyes.poo.herencia.Profesor;
 
 public class AppHerencia {
@@ -9,6 +10,23 @@ public class AppHerencia {
     Alumno alumno = new Alumno();
     alumno.setNombre("Jolberth");
     alumno.setApellido("Reyes");
+    alumno.setInstitucion("Instituto Nacional");
+    alumno.setNotaCastellano(5.5);
+    alumno.setNotaHistoria(6.3);
+    alumno.setNotaMatematica(4.9);
+    
+    
+    AlumnoInternacional alumnoInt = new AlumnoInternacional();
+    alumnoInt.setNombre("Peter");
+    alumnoInt.setApellido("Gosling");
+    alumnoInt.setPais("Australia");
+    alumnoInt.setEdad(15);
+    alumnoInt.setInstitucion("Instituto Nacional");
+    alumnoInt.setNotaIdiomas(6.8);
+    alumnoInt.setNotaCastellano(6.2);
+    alumnoInt.setNotaHistoria(5.8);
+    alumnoInt.setNotaMatematica(6.5);
+    
     
     Profesor profesor = new Profesor();
     profesor.setNombre("Roberto");
@@ -16,6 +34,16 @@ public class AppHerencia {
     profesor.setAsignatura("Matematicas");
     
     System.out.println("Alumno "+alumno.getNombre()+" "+alumno.getApellido());
+    System.out.println("Alumno Internacional"+alumnoInt.getNombre()+" "+alumnoInt.getApellido()+" "+alumnoInt.getInstitucion()+" "+alumnoInt.getPais());
     System.out.println("Profesor "+profesor.getAsignatura()+": "+profesor.getNombre()+" "+profesor.getApellido());
+    
+    System.out.println();
+    Class clase = alumnoInt.getClass();
+    while (clase.getSuperclass() != null) {      
+      String hija = clase.getName();
+      String padre = clase.getSuperclass().getName();
+      System.out.println("["+hija+"] es una clase hija de la clase padre ["+padre+"]");
+      clase = clase.getSuperclass();
+    }
   }  
 }
