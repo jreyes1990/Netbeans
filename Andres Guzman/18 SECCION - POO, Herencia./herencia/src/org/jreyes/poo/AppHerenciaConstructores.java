9956e6aac96215b464cf5cd699fe8cf0c83b300a
jreyes1990 <jarl1990mt@gmail.com>
2024-01-15 22:31:38 -0600

    184. Creando instancias vía constructores con argumentos y control de tipos

---
+++ Andres Guzman/18 SECCION - POO, Herencia./herencia/src/org/jreyes/poo/AppHerenciaConstructores.java
@@ -0,0 +1,51 @@
+package org.jreyes.poo;
+
+import org.jreyes.poo.herencia.Alumno;
+import org.jreyes.poo.herencia.AlumnoInternacional;
+import org.jreyes.poo.herencia.Profesor;
+
+public class AppHerencia {
+
+  public static void main(String[] args) {
+    System.out.println("***** CREANDO LA INSTANCIA DE LA CLASE ALUMNO *****");
+    Alumno alumno = new Alumno();
+    alumno.setNombre("Jolberth");
+    alumno.setApellido("Reyes");
+    alumno.setInstitucion("Instituto Nacional");
+    alumno.setNotaCastellano(5.5);
+    alumno.setNotaHistoria(6.3);
+    alumno.setNotaMatematica(4.9);
+    
+    System.out.println("\n***** CREANDO LA INSTANCIA DE LA CLASE ALUMNO INTERNACIONAL *****");
+    AlumnoInternacional alumnoInt = new AlumnoInternacional();
+    alumnoInt.setNombre("Peter");
+    alumnoInt.setApellido("Gosling");
+    alumnoInt.setPais("Australia");
+    alumnoInt.setEdad(15);
+    alumnoInt.setInstitucion("Instituto Nacional");
+    alumnoInt.setNotaIdiomas(6.8);
+    alumnoInt.setNotaCastellano(6.2);
+    alumnoInt.setNotaHistoria(5.8);
+    alumnoInt.setNotaMatematica(6.5);
+    
+    System.out.println("\n***** CREANDO LA INSTANCIA DE LA CLASE PROFESOR *****");
+    Profesor profesor = new Profesor();
+    profesor.setNombre("Roberto");
+    profesor.setApellido("Osorio");
+    profesor.setAsignatura("Matematicas");
+    
+    System.out.println("\n************************************************************");
+    System.out.println("Alumno "+alumno.getNombre()+" "+alumno.getApellido());
+    System.out.println("Alumno Internacional"+alumnoInt.getNombre()+" "+alumnoInt.getApellido()+" "+alumnoInt.getInstitucion()+" "+alumnoInt.getPais());
+    System.out.println("Profesor "+profesor.getAsignatura()+": "+profesor.getNombre()+" "+profesor.getApellido());
+    
+    System.out.println();
+    Class clase = alumnoInt.getClass();
+    while (clase.getSuperclass() != null) {      
+      String hija = clase.getName();
+      String padre = clase.getSuperclass().getName();
+      System.out.println("["+hija+"] es una clase hija de la clase padre ["+padre+"]");
+      clase = clase.getSuperclass();
+    }
+  }  
+}
