96351e95a5b51e93f93446477fe164a4c49e25cc
jreyes1990 <jarl1990mt@gmail.com>
2024-01-21 15:04:51 -0600

    202. Implementando la validación en la clase abstracta ElementoForm

--- Andres Guzman/19 SECCION - POO, Herencia y clase Abstracta/clase_abstracta/src/org/jreyes/poo/clase_abstracta/form/elementos/ElementoForm.java
+++ Andres Guzman/19 SECCION - POO, Herencia y clase Abstracta/clase_abstracta/src/org/jreyes/poo/clase_abstracta/form/elementos/ElementoForm.java
@@ -1,10 +1,19 @@
 package org.jreyes.poo.clase_abstracta.form.elementos;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.jreyes.poo.clase_abstracta.form.validador.Validador;
+
 abstract public class ElementoForm {
   protected String valor;
   protected String nombre;
+  
+  private List<Validador> validadores;
+  private List<String> errores;
 
   public ElementoForm() {
+    this.validadores = new ArrayList<>();
+    this.errores = new ArrayList<>();
   }
 
   public ElementoForm(String nombre) {
@@ -12,8 +21,26 @@ abstract public class ElementoForm {
     this.nombre = nombre;
   }
 
+  public ElementoForm addValidador(Validador validador){
+    this.validadores.add(validador);
+    return this;
+  }
+
+  public List<String> getErrores() {
+    return errores;
+  }
+  
   public void setValor(String valor) {
     this.valor = valor;
+  }
+  
+  public boolean esValido(){
+    for (Validador v : validadores) {
+      if (!v.esValido(this.valor)) {
+        this.errores.add(v.getMensaje());
+      }
+    }
+    return this.errores.isEmpty();
   }
   
   abstract public String dibujarHtml();
