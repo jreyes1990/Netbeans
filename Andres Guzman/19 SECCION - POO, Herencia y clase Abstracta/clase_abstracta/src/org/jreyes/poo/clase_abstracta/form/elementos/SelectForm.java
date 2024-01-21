ee68f3d8159d7d582f6126fdf2e7e821d8fe1d1c
jreyes1990 <jarl1990mt@gmail.com>
2024-01-20 22:20:30 -0600

    195. Creando la clase concreta SelectForm

---
+++ Andres Guzman/19 SECCION - POO, Herencia y clase Abstracta/clase_abstracta/src/org/jreyes/poo/clase_abstracta/form/elementos/SelectForm.java
@@ -0,0 +1,43 @@
+package org.jreyes.poo.clase_abstracta.form.elementos;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jreyes.poo.clase_abstracta.form.elementos.select.Opcion;
+
+public class SelectForm extends ElementoForm {
+  private List<Opcion> opciones;
+
+  public SelectForm(String nombre) {
+    super(nombre);
+    this.opciones = new ArrayList<Opcion>();
+  }
+
+  public SelectForm(String nombre, List<Opcion> opciones) {
+    super(nombre);
+    this.opciones = opciones;
+  }
+  
+  public SelectForm addOpcion(Opcion opcion){
+    this.opciones.add(opcion);
+    return this;
+  }
+  
+  @Override
+  public String dibujarHtml() {
+    StringBuilder sb = new StringBuilder("<select ");
+    sb.append("name='").append(this.nombre).append("'>");
+    
+    for (Opcion opcion : opciones) {
+      sb.append("\n<option value='").append(opcion.getValor()).append("' ");
+      if (opcion.isSelected()) {
+        sb.append("selected");
+      }
+      sb.append(">").append(opcion.getNombre()).append("</option>");
+      
+    }
+    sb.append("</select>");
+    
+    return sb.toString();
+  }
+  
+}
