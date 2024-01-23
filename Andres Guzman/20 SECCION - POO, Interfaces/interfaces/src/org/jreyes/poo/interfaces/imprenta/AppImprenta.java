b2458b9a77a073df4dae087ac9a67133f8fabfde
jreyes1990 <jarl1990mt@gmail.com>
2024-01-22 21:30:59 -0600

    211. Añadiendo la clase Libro y la interface

--- Andres Guzman/20 SECCION - POO, Interfaces/interfaces/src/org/jreyes/poo/interfaces/imprenta/AppImprenta.java
+++ Andres Guzman/20 SECCION - POO, Interfaces/interfaces/src/org/jreyes/poo/interfaces/imprenta/AppImprenta.java
@@ -1,8 +1,11 @@
 package org.jreyes.poo.interfaces.imprenta;
 
 import org.jreyes.poo.interfaces.imprenta.modelo.Curriculum;
+import org.jreyes.poo.interfaces.imprenta.modelo.Genero;
 import org.jreyes.poo.interfaces.imprenta.modelo.Hoja;
 import org.jreyes.poo.interfaces.imprenta.modelo.Informe;
+import org.jreyes.poo.interfaces.imprenta.modelo.Libro;
+import org.jreyes.poo.interfaces.imprenta.modelo.Pagina;
 
 public class AppImprenta {
 
@@ -14,7 +17,15 @@ public class AppImprenta {
     cv.addExperiencia("Desarrollador FullStack");
     cv.addExperiencia("Angular");
     
+    Libro libro = new Libro("Erick Gamma", "Patrones de disenio: Elem. Reusables POO", Genero.PROGRAMACION);
+    libro.addPagina(new Pagina("Patron Singleton"))
+         .addPagina(new Pagina("Patron Observador"))
+         .addPagina(new Pagina("Patron Factory"))
+         .addPagina(new Pagina("Patron Compisite"))
+         .addPagina(new Pagina("Patron Facade"));
+    
     Informe informe = new Informe("James", "Martin Fowler", "Estudio sobre microservicios");
+    
     imprimir(cv);
     imprimir(informe);
   } 
