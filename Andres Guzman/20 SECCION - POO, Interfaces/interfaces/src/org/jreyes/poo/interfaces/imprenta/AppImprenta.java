8ba143dfda5658754df04c0e4b83bff0610c6bc1
jreyes1990 <jarl1990mt@gmail.com>
2024-01-22 21:47:28 -0600

    211. Añadiendo la clase Libro y la interface

--- Andres Guzman/20 SECCION - POO, Interfaces/interfaces/src/org/jreyes/poo/interfaces/imprenta/AppImprenta.java
+++ Andres Guzman/20 SECCION - POO, Interfaces/interfaces/src/org/jreyes/poo/interfaces/imprenta/AppImprenta.java
@@ -2,7 +2,7 @@ package org.jreyes.poo.interfaces.imprenta;
 
 import org.jreyes.poo.interfaces.imprenta.modelo.Curriculum;
 import org.jreyes.poo.interfaces.imprenta.modelo.Genero;
-import org.jreyes.poo.interfaces.imprenta.modelo.Hoja;
+// import org.jreyes.poo.interfaces.imprenta.modelo.Hoja;
 import org.jreyes.poo.interfaces.imprenta.modelo.Imprimible;
 import org.jreyes.poo.interfaces.imprenta.modelo.Informe;
 import org.jreyes.poo.interfaces.imprenta.modelo.Libro;
@@ -17,21 +17,21 @@ public class AppImprenta {
     cv.addExperiencia("Spring Framework");
     cv.addExperiencia("Desarrollador FullStack");
     cv.addExperiencia("Angular");
-    
+
     Libro libro = new Libro("Erick Gamma", "Patrones de disenio: Elem. Reusables POO", Genero.PROGRAMACION);
     libro.addPagina(new Pagina("Patron Singleton"))
-         .addPagina(new Pagina("Patron Observador"))
-         .addPagina(new Pagina("Patron Factory"))
-         .addPagina(new Pagina("Patron Compisite"))
-         .addPagina(new Pagina("Patron Facade"));
-    
+        .addPagina(new Pagina("Patron Observador"))
+        .addPagina(new Pagina("Patron Factory"))
+        .addPagina(new Pagina("Patron Compisite"))
+        .addPagina(new Pagina("Patron Facade"));
+
     Informe informe = new Informe("James", "Martin Fowler", "Estudio sobre microservicios");
-    
+
     imprimir(cv);
     imprimir(informe);
-  } 
-  
-  public static void imprimir(Imprimible imprimible){
+  }
+
+  public static void imprimir(Imprimible imprimible) {
     System.out.println(imprimible.imprimir());
   }
 }
