8e290a6033c4f97346b400e0a8899b563512dc36
jreyes1990 <jarl1990mt@gmail.com>
2024-01-27 23:11:49 -0600

    217. Implementando métodos de interfaces para ordenar y paginar

--- Andres Guzman/21 SECCION - POO, Ejemplo Interfaces Repositorio/interfaces_repositorio/src/org/poo/repositorio/implementos/ClienteListRepositorio.java
+++ Andres Guzman/21 SECCION - POO, Ejemplo Interfaces Repositorio/interfaces_repositorio/src/org/poo/repositorio/implementos/ClienteListRepositorio.java
@@ -1,6 +1,7 @@
 package org.poo.repositorio.implementos;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import org.poo.repositorio.interfaces.CrudRepositorio;
 import org.poo.repositorio.interfaces.Direccion;
@@ -9,6 +10,7 @@ import org.poo.repositorio.interfaces.PaginableRepositorio;
 import org.poo.repositorio.modelo.Cliente;
 
 public class ClienteListRepositorio implements CrudRepositorio, OrdenableRepositorio, PaginableRepositorio {
+
   private List<Cliente> dataSource;
 
   public ClienteListRepositorio() {
@@ -23,14 +25,14 @@ public class ClienteListRepositorio implements CrudRepositorio, OrdenableReposit
   @Override
   public Cliente porId(Integer id) {
     Cliente resultado = null;
-    
+
     for (Cliente cli : dataSource) {
       if (cli.getId().equals(id)) {
         resultado = cli;
         break;
       }
     }
-    
+
     return resultado;
   }
 
@@ -54,12 +56,43 @@ public class ClienteListRepositorio implements CrudRepositorio, OrdenableReposit
 
   @Override
   public List<Cliente> listar(String campo, Direccion dir) {
-    return null;
+    dataSource.sort((a, b) -> {
+      int resultado = 0;
+
+      if (dir == Direccion.ASC) {
+        switch (campo) {
+          case "id":
+            resultado = a.getId().compareTo(b.getId());
+            break;
+          case "nombre":
+            resultado = a.getNombre().compareTo(b.getNombre());
+            break;
+          case "apellido":
+            resultado = a.getApellido().compareTo(b.getApellido());
+            break;
+        }
+      } else if (dir == Direccion.DESC) {
+        switch (campo) {
+          case "id":
+            resultado = b.getId().compareTo(a.getId());
+            break;
+          case "nombre":
+            resultado = b.getNombre().compareTo(a.getNombre());
+            break;
+          case "apellido":
+            resultado = b.getApellido().compareTo(a.getApellido());
+            break;
+        }
+      }
+      return resultado;
+    });
+
+    return dataSource;
   }
 
   @Override
   public List<Cliente> listar(int desde, int hasta) {
-    return null;
+    return dataSource.subList(desde, hasta);
   }
-  
+
 }
