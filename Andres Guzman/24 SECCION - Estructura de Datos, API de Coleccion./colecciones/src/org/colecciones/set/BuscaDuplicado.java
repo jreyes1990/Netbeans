ca44b2156543ecbd45517d3fc6113185c5058b9a
jreyes1990 <jarl1990mt@gmail.com>
2024-01-29 23:07:40 -0600

    242. Ejemplo HashSet detectando duplicados

---
+++ Andres Guzman/24 SECCION - Estructura de Datos, API de Coleccion./colecciones/src/org/colecciones/set/BuscaDuplicado.java
@@ -0,0 +1,23 @@
+package org.colecciones.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class BuscaDuplicado {
+
+  public static void main(String[] args) {
+    String[] peces = {"Corvina", "Lenguado", "Pejerreye", "Robalo", "Atun", "Lenguado", "Corvina"};
+    Set<String> unicos = new HashSet<>();
+    Set<String> duplicados = new HashSet<>();
+    
+    for (String pez : peces) {
+      if (!unicos.add(pez)) {
+        duplicados.add(pez);
+      }
+    }
+    unicos.removeAll(duplicados);
+    
+    System.out.println("Unicos: "+unicos);
+    System.out.println("Duplicados: "+duplicados);
+  }  
+}
