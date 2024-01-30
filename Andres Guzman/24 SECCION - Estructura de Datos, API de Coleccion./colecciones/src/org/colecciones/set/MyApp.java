f198adb580e8464fb061e60f378dd79270f0165a
jreyes1990 <jarl1990mt@gmail.com>
2024-01-29 22:38:17 -0600

    241. Ejemplo HashSet

---
+++ Andres Guzman/24 SECCION - Estructura de Datos, API de Coleccion./colecciones/src/org/colecciones/set/MyApp.java
@@ -0,0 +1,22 @@
+package org.colecciones.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MyApp {
+
+  public static void main(String[] args) {
+    Set<String> hs = new HashSet<>();
+    hs.add("uno");
+    hs.add("dos");
+    hs.add("tres");
+    hs.add("cuatro");
+    hs.add("cinco");
+    
+    System.out.println(hs);
+    
+    boolean b = hs.add("tres");
+    System.out.println("Permite elementos duplicados? "+b);
+    System.out.println(hs);
+  }  
+}
