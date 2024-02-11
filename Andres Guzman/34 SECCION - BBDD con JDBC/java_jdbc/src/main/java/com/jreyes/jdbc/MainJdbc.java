73c366ba4d93c0bfcff0f7e8c711ec167c609e7e
jreyes1990 <jarl1990mt@gmail.com>
2024-02-10 23:11:22 -0600

    340. Añadiendo la clase singleton de conexión a la base de datos

--- Andres Guzman/34 SECCION - BBDD con JDBC/java_jdbc/src/main/java/com/jreyes/jdbc/MainJdbc.java
+++ Andres Guzman/34 SECCION - BBDD con JDBC/java_jdbc/src/main/java/com/jreyes/jdbc/MainJdbc.java
@@ -1,5 +1,6 @@
 package com.jreyes.jdbc;
 
+import com.jreyes.jdbc.util.ConexionBD;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,12 +9,8 @@ import java.sql.Statement;
 
 public class MainJdbc {
   
-  public static void main(String[] args) {
-    String url = "jdbc:mysql://localhost:3306/java_curso?useSSL=false&serverTimezone=UTC";
-    String user = "root";
-    String password = "root";
-    
-    try (Connection conn = DriverManager.getConnection(url, user, password);
+  public static void main(String[] args) { 
+    try (Connection conn = ConexionBD.getInstance();
          Statement stmt = conn.createStatement();
          ResultSet resultado = stmt.executeQuery("select * from productos");
         ) {
---
+++ Andres Guzman/34 SECCION - BBDD con JDBC/java_jdbc/src/main/java/com/jreyes/jdbc/util/ConexionBD.java
@@ -0,0 +1,21 @@
+package com.jreyes.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+  private static String url = "jdbc:mysql://localhost:3306/java_curso?useSSL=false&serverTimezone=UTC";
+  private static String user = "root";
+  private static String password = "root";
+  private static Connection connection;
+
+  public static Connection getInstance() throws SQLException {
+    if (connection == null) {
+      connection = DriverManager.getConnection(url, user, password);
+    }
+    
+    return connection;
+  }
+  
+}
