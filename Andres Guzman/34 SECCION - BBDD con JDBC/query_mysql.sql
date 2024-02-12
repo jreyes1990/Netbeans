44376e7c5dbe9471982387b4fa076e36e56be2fa
jreyes1990 <jarl1990mt@gmail.com>
2024-02-11 18:16:04 -0600

    346. Relaciones de tablas creando esquema DDL

--- Andres Guzman/34 SECCION - BBDD con JDBC/query_mysql.sql
+++ Andres Guzman/34 SECCION - BBDD con JDBC/query_mysql.sql
@@ -27,5 +27,32 @@
 		primary key(id)
 	);
 
--- 	INSERTANDO LOS PRIMERO REGISTROS EN LA TABLA PRODUCTOS DE BASE DE DATOS 'java_curso'
+	create table categorias(
+		id 				int 		not null auto_increment,
+		nombre			varchar(45)	null,
+		primary key(id)
+	);
+
+-- 	ALTERNANDO TABLAS EN LA BASE DE DATOS 'java_curso'
+	-- AGREGANDO COLUMNA(S)
+	alter table productos add column categoria_id int null after fecha_registro;
+
+	-- AGREGANDO INDEX(ES)
+	alter table productos add index idx_productos_categorias (categoria_id asc) visible;	
+
+	-- AGREGANDO FOREIGN KEY(S)
+	alter table productos add constraint fk_productos_categorias foreign key (categoria_id) references categorias (id) on delete no action on update no action;
+
+-- 	INSERTANDO REGISTROS EN LA TABLA PRODUCTOS DE BASE DE DATOS 'java_curso'
 	insert into productos(nombre, precio, fecha_registro) values('Bicicleta', 200, '2020-12-10'), ('Samsung TV', 300, '2021-05-29');
+
+-- 	INSERTANDO REGISTROS EN LA TABLA CATEGORIAS DE BASE DE DATOS 'java_curso'
+	insert into categorias(nombre) values('Deporte'), ('Tecnologia'), ('Computacion');
+	
+-- 	ACTUALIZANDO REGISTROS EN LA TABLA CATEGORIAS DE BASE DE DATOS 'java_curso'
+	update productos set categoria_id=1 where id=1;
+	update productos set categoria_id=2 where id=2;
+	commit;
+
+-- 	CONSULTANDO TABLAS DE BASE DE DATOS 'java_curso'
+	select * from productos p inner join categorias c on(p.categoria_id=c.id);
