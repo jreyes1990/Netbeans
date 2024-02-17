ef332ca60fc94951ffff5c2a353dd67249be0c2a
jreyes1990 <jarl1990mt@gmail.com>
2024-02-16 23:25:01 -0600

    356. Ejemplo Transacciones con pool de conexiones parte 3 (obtener último insert id)

--- Andres Guzman/36 SECCION - BBDD con JDBC, Transacciones/java_jdbc_pool_trx/src/main/java/com/jreyes/jdbc/repositorio/CategoriaRepositorioImpl.java
+++ Andres Guzman/36 SECCION - BBDD con JDBC, Transacciones/java_jdbc_pool_trx/src/main/java/com/jreyes/jdbc/repositorio/CategoriaRepositorioImpl.java
@@ -50,12 +50,42 @@ public class CategoriaRepositorioImpl implements Repositorio<Categoria>{
   }
 
   @Override
-  public Categoria guardar(Categoria t) throws SQLException {
-    return null;
+  public Categoria guardar(Categoria categoria) throws SQLException {
+    String sql = null;
+    
+    if (categoria.getId() != null && categoria.getId() > 0) {
+      sql = "update categorias set nombre=? where id=?";
+    } else {
+      sql = "insert into categorias(nombre) values(?)";
+    }
+    
+    try (PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+      stmt.setString(1, categoria.getNombre());
+      
+      if (categoria.getId() != null && categoria.getId() > 0) {
+        stmt.setLong(1, categoria.getId());
+      }
+      
+      stmt.executeUpdate();
+      
+      if (categoria.getId() == null) {
+        try (ResultSet rs = stmt.getGeneratedKeys()) {
+          if (rs.next()) {
+            categoria.setId(rs.getLong(1));
+          }
+        }
+      }
+    }
+    
+    return categoria;
   }
 
   @Override
   public void eliminar(Long id) throws SQLException {
+    try (PreparedStatement stmt = conn.prepareStatement("delete from categorias where id = ?")) {
+      stmt.setLong(1, id);
+      stmt.executeUpdate();
+    }
   }
   
   private Categoria crearCategoria(final ResultSet rs) throws SQLException {
