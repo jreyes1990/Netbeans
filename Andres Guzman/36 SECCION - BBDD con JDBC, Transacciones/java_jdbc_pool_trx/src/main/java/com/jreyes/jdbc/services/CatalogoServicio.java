ce22206fdd2cfa11e495debeae5fc7838352abab
jreyes1990 <jarl1990mt@gmail.com>
2024-02-18 17:32:05 -0600

    359. Implementando los metodos de la clase Service

--- Andres Guzman/36 SECCION - BBDD con JDBC, Transacciones/java_jdbc_pool_trx/src/main/java/com/jreyes/jdbc/services/CatalogoServicio.java
+++ Andres Guzman/36 SECCION - BBDD con JDBC, Transacciones/java_jdbc_pool_trx/src/main/java/com/jreyes/jdbc/services/CatalogoServicio.java
@@ -24,29 +24,56 @@ public class CatalogoServicio implements Servicio {
     try (Connection conn = ConexionBD.getConnection()) {
       productoRepositorio.setConn(conn);
     } 
-    return null;
+    return productoRepositorio.listar();
   }
 
   @Override
   public Producto porIdProducto(Long id) throws SQLException {
     try (Connection conn = ConexionBD.getConnection()) {
       productoRepositorio.setConn(conn);
+      return productoRepositorio.porId(id);
     }
-    return null;
   }
 
   @Override
   public Producto guardarProducto(Producto producto) throws SQLException {
     try (Connection conn = ConexionBD.getConnection()) {
       productoRepositorio.setConn(conn);
+      
+      if (conn.getAutoCommit()) {
+        conn.setAutoCommit(false);
+      }
+      
+      Producto nuevoProducto = null;
+      
+      try {
+        nuevoProducto = productoRepositorio.guardar(producto);
+        conn.commit();
+      } catch (SQLException e) {
+        conn.rollback();
+        e.printStackTrace();
+      }
+      
+      return nuevoProducto;
     }
-    return null;
   }
 
   @Override
   public void eliminarProducto(Long id) throws SQLException {
     try (Connection conn = ConexionBD.getConnection()) {
       productoRepositorio.setConn(conn);
+      
+      if (conn.getAutoCommit()) {
+        conn.setAutoCommit(false);
+      }
+      
+      try {
+        productoRepositorio.eliminar(id);
+        conn.commit();
+      } catch (SQLException e) {
+        conn.rollback();
+        e.printStackTrace();
+      }
     }
   }
 
@@ -54,30 +81,58 @@ public class CatalogoServicio implements Servicio {
   public List<Categoria> listarCategoria() throws SQLException {
     try (Connection conn = ConexionBD.getConnection()) {
       categoriaRepositorio.setConn(conn);
+      return categoriaRepositorio.listar();
     }
-    return null;
   }
 
   @Override
   public Categoria porIdCategoria(Long id) throws SQLException {
     try (Connection conn = ConexionBD.getConnection()) {
       categoriaRepositorio.setConn(conn);
+      
+      return categoriaRepositorio.porId(id);
     }
-    return null;
   }
 
   @Override
-  public Categoria guardarCategori(Categoria categoria) throws SQLException {
+  public Categoria guardarCategoria(Categoria categoria) throws SQLException {
     try (Connection conn = ConexionBD.getConnection()) {
       categoriaRepositorio.setConn(conn);
+      
+      if (conn.getAutoCommit()) {
+        conn.setAutoCommit(false);
+      }
+      
+      Categoria nuevaCategoria = null;
+      
+      try {
+        nuevaCategoria = categoriaRepositorio.guardar(categoria);
+        conn.commit();
+      } catch (SQLException e) {
+        conn.rollback();
+        e.printStackTrace();
+      }
+      
+      return nuevaCategoria;
     }
-    return null;
   }
 
   @Override
   public void eliminarCategoria(Long id) throws SQLException {
     try (Connection conn = ConexionBD.getConnection()) {
       categoriaRepositorio.setConn(conn);
+      
+      if (conn.getAutoCommit()) {
+        conn.setAutoCommit(false);
+      }
+      
+      try {
+        categoriaRepositorio.eliminar(id);
+        conn.commit();
+      } catch (SQLException e) {
+        conn.rollback();
+        e.printStackTrace();
+      }
     }
   }
 
@@ -86,6 +141,20 @@ public class CatalogoServicio implements Servicio {
     try (Connection conn = ConexionBD.getConnection()) {
       productoRepositorio.setConn(conn);
       categoriaRepositorio.setConn(conn);
+      
+      if (conn.getAutoCommit()) {
+        conn.setAutoCommit(false);
+      }
+      
+      try {
+        Categoria nuevaCategoria = categoriaRepositorio.guardar(categoria);
+        producto.setCategoria(nuevaCategoria);
+        productoRepositorio.guardar(producto);
+        conn.commit();
+      } catch (SQLException e) {
+        conn.rollback();
+        e.printStackTrace();
+      }
     }
   }
   
--- Andres Guzman/36 SECCION - BBDD con JDBC, Transacciones/java_jdbc_pool_trx/src/main/java/com/jreyes/jdbc/services/Servicio.java
+++ Andres Guzman/36 SECCION - BBDD con JDBC, Transacciones/java_jdbc_pool_trx/src/main/java/com/jreyes/jdbc/services/Servicio.java
@@ -12,7 +12,7 @@ public interface Servicio {
   public void eliminarProducto(Long id) throws SQLException;
   public List<Categoria> listarCategoria() throws SQLException;
   public Categoria porIdCategoria(Long id) throws SQLException;
-  public Categoria guardarCategori(Categoria categoria) throws SQLException;
+  public Categoria guardarCategoria(Categoria categoria) throws SQLException;
   public void eliminarCategoria(Long id) throws SQLException;
   public void guardarProductoCategoria(Producto producto, Categoria categoria) throws SQLException;
 }
