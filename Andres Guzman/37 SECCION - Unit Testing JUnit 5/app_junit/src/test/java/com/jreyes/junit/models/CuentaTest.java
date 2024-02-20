da30e0e92401fd84e0a51f7378967da328e5a7ed
jreyes1990 <jarl1990mt@gmail.com>
2024-02-19 20:02:21 -0600

    366. Escribiendo test para el saldo

--- Andres Guzman/37 SECCION - Unit Testing JUnit 5/app_junit/src/test/java/com/jreyes/junit/models/CuentaTest.java
+++ Andres Guzman/37 SECCION - Unit Testing JUnit 5/app_junit/src/test/java/com/jreyes/junit/models/CuentaTest.java
@@ -15,4 +15,12 @@ public class CuentaTest {
     assertEquals(esperado, real);
     assertTrue(real.equals("Andres"));
   }
+  
+  @Test
+  public void testSaldoCuenta(){
+    Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+    assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+    assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+    assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+  }
 }
