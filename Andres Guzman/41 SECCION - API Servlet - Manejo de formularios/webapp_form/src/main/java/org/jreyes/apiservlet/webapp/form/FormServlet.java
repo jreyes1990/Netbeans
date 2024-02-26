1fb7640582e698f71b629a5000cb6661fcc7cb41
jreyes1990 <jarl1990mt@gmail.com>
2024-02-25 22:47:04 -0600

    413. Agregando otros campos de radio button, checkbox y hidden

--- Andres Guzman/41 SECCION - API Servlet - Manejo de formularios/webapp_form/src/main/java/org/jreyes/apiservlet/webapp/form/FormServlet.java
+++ Andres Guzman/41 SECCION - API Servlet - Manejo de formularios/webapp_form/src/main/java/org/jreyes/apiservlet/webapp/form/FormServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 
-@WebServlet("/registro")
+@WebServlet("/webapp-form/registro")
 public class FormServlet extends HttpServlet {
 
   @Override
@@ -21,6 +21,9 @@ public class FormServlet extends HttpServlet {
     String pais = req.getParameter("pais");
     String[] lenguajes = req.getParameterValues("lenguajes");
     String[] roles = req.getParameterValues("roles");
+    String idioma = req.getParameter("idioma");
+    String habilitar = req.getParameter("habilitar");
+    String secreto = req.getParameter("secreto");
     
     try (PrintWriter out = resp.getWriter()) {
       out.println("<!DOCTYPE html>");
@@ -36,7 +39,7 @@ public class FormServlet extends HttpServlet {
       out.println("       <li>Username: "+ username + "</li>");
       out.println("       <li>Password: "+ password + "</li>");
       out.println("       <li>Email: "+ email + "</li>");
-      out.println("       <li>Pais</li>");
+      out.println("       <li>Pais: "+ pais +"</li>");
       out.println("       <li>Lenguajes: ");
       out.println("         <ul>");
       Arrays.asList(lenguajes).forEach(lenguaje -> {
@@ -49,6 +52,9 @@ public class FormServlet extends HttpServlet {
       });
       out.println("         </ul>");
       out.println("       </li>");
+      out.println("       <li>Idioma: "+ idioma + "</li>");
+      out.println("       <li>Habilitado: "+ habilitar + "</li>");
+      out.println("       <li>Secreto: "+ secreto + "</li>");
       out.println("     </ul>");
       out.println("   </body>");
       out.println("</html>");
