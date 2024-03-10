1333a18810f93b87ad5250e240c63827ca8fa769
jreyes1990 <jarl1990mt@gmail.com>
2024-03-09 22:51:01 -0600

    421. Cabeceras HTTP del request

---
+++ Andres Guzman/42 SECCION - API Servlet - Cabeceras HTTP y Status/webapp_headers/src/main/java/com/jreyes/apiservlet/webapp/headers/webapp_headers/CabeceraHttpRequestServlet.java
@@ -0,0 +1,66 @@
+package com.jreyes.apiservlet.webapp.headers.webapp_headers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+@WebServlet("/cabeceras-request")
+public class CabeceraHttpRequestServlet extends HttpServlet{
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setContentType("text/html; charset=UTF-8");
+    
+    String metodoHttp   = req.getMethod();
+    String requestUri   = req.getRequestURI();
+    String requestUrl   = req.getRequestURL().toString();
+    String contextPath  = req.getContextPath();
+    String servletPath  = req.getServletPath();
+    String ipCliente    = req.getRemoteAddr();
+    String ip           = req.getLocalAddr();
+    int port            = req.getLocalPort();
+    String scheme       = req.getScheme();
+    String host         = req.getHeader("host");
+    String url          = scheme+"://"+ host + contextPath + servletPath;
+    String url2         = scheme+"://"+ ip + ":" + port + contextPath + servletPath;
+    
+    try (PrintWriter out = resp.getWriter()) {
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang=\"en\">");
+        out.println("   <head>");
+        out.println("     <meta charset+\"UTF-8\">");
+        out.println("     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+        out.println("     <title>Cabeceras HTTP Request</title>");
+        out.println("   </head>");
+        out.println("   <body>");
+        out.println("     <h1>Cabeceras HTTP Request!</h1>");
+        out.println("     <ul>");
+        out.println("       <li>Metodo Http: "+ metodoHttp + "</li>");
+        out.println("       <li>Request URI: "+ requestUri + "</li>");
+        out.println("       <li>Request URL: "+ requestUrl + "</li>");
+        out.println("       <li>Context Path: "+ contextPath + "</li>");
+        out.println("       <li>Servlet Path: "+ servletPath + "</li>");
+        out.println("       <li>IP Cliente: "+ ipCliente + "</li>");
+        out.println("       <li>IP Local: "+ ip + "</li>");
+        out.println("       <li>Puerto Local: "+ port + "</li>");
+        out.println("       <li>Scheme: "+ scheme + "</li>");
+        out.println("       <li>Host: "+ host + "</li>");
+        out.println("       <li>url: "+ url + "</li>");
+        out.println("       <li>url2: "+ url2 + "</li>");
+        Enumeration<String> headersNames = req.getHeaderNames();
+        while (headersNames.hasMoreElements()) {          
+          String cabecera = headersNames.nextElement();
+        out.println("       <li>"+ cabecera + ": " + req.getHeader(cabecera) +"</li>");  
+        }
+        out.println("     </ul>");
+        out.println("   </body>");
+        out.println("</html>");
+      }
+  }
+  
+}
---
+++ Andres Guzman/42 SECCION - API Servlet - Cabeceras HTTP y Status/webapp_headers/src/main/java/com/jreyes/apiservlet/webapp/headers/webapp_headers/Webapp_headers.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.jreyes.apiservlet.webapp.headers.webapp_headers;
+
+/**
+ *
+ * @author jreyes
+ */
+public class Webapp_headers {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
